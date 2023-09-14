6fbc2ec4aa195f0ce5b04c8562569731512d1668
Júllia Izidorio Paiva Silva <81257067+JULLIAIP@users.noreply.github.com>
2023-09-14 11:24:37 -0300

    cors config

--- upload-server/src/server/server.ts
+++ upload-server/src/server/server.ts
@@ -1,10 +1,15 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 import { getAllPrompts } from '../routes/getAllPrompts'
 import { uploadVideo } from '../routes/uploadVideo'
 import { createTranscription } from '../routes/createTranscription'
 import { generateAI } from '../routes/generateAI'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 //Routes
 app.register(getAllPrompts)
